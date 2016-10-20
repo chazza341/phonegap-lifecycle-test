29ed3eb183945e86d9187847af9cb05dacdbde3c
chazza341 <LESC1_15@uni.worc.ac.uk>
2016-10-20 10:29:03 +0100

    Simplifying Code

--- www/js/index.js
+++ www/js/index.js
@@ -1,41 +1,10 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
 
     function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-		console.log("device ready");
-    }
-	
-	function updateDisplay() {
-		$("#launched").text("Application launched: " + launched_count);
-		$("#resumed").text("Application paused: " + paused_count);
-		$("#paused").text("Application resumed: " + resumed_count);
-	}
-
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-		alert("device ready");
         
-		document.addEventListener("resume", onResume, false);
-		document.addEventListener("pause", onPause, false);
-		
-		launched_count++;
-		updateDisplay();
-    }
-
-    // Handle the pause event
-    //
-    function onPause() {
-		alert("pause");
-		paused_count++;
-		updateDisplay();
+		console.log("device ready");
+		var key = “pen”;
+		var value = “blue”;
+		window.localStorage.setItem( key, value );
     }
 	
-	function onResume() {
-		alert("resume");
-		resumed_count++;
-		updateDisplay();
-    }
+	
